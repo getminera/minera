ad0f30d908389646cfb3614fd0aaad2fc94e7d73
Michele <michele@befree.it>
2014-05-16 12:00:36 +0000

    put jquery local on lockscreen too

--- application/views/lockscreen.php
+++ application/views/lockscreen.php
@@ -38,7 +38,7 @@
 			
         </div><!-- /.center -->
 
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+		<script src="<?php echo base_url('assets/js/jquery-2.1.1.min.js') ?>"></script>
 		<script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
 
         <!-- page script -->
