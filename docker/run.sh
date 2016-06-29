2aa2ac457c18b620301ae0ad5b9422154e45713c
James Cuzella <james.cuzella@lyraphase.com>
2016-06-29 02:32:01 -0600

    Publish shellinabox port 4200 for the browser terminal to work

--- docker/run.sh
+++ docker/run.sh
@@ -8,5 +8,6 @@
 docker run --rm --link redis -e container=docker --cap-add SYS_ADMIN --privileged  -it -v /run -v /run/lock -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
   -p 443:443 \
   -p 80:80 \
+  -p 4200:4200 \
   trinitronx/minera
 
