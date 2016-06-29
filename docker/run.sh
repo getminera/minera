21cec118c7c3447a80ae31974d7290c16714832b
James Cuzella <james.cuzella@lyraphase.com>
2016-06-29 01:40:41 -0600

    First pass at Dockerizing Minera

---
+++ docker/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#if docker ps -a | grep -q redis; then
+#  docker kill redis; docker rm -v redis
+#fi
+
+#docker run  --name redis -d redis
+docker run --rm --link redis -e container=docker --cap-add SYS_ADMIN --privileged  -it -v /run -v /run/lock -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
+  -p 443:443 \
+  -p 80:80 \
+  trinitronx/minera
+
