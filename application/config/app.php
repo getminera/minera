303ce072918727dea1a88ebfb3a73d2f96510d27
Minera <support@getminera.com>
2017-09-24 17:26:24 +0000

    Release 0.8.1

--- application/config/app.php
+++ application/config/app.php
@@ -1,8 +1,8 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 // App specific configs
-$config['ENV'] = 'development';
-//$config['ENV'] = 'production';
+//$config['ENV'] = 'development';
+$config['ENV'] = 'production';
 $config['minera_api_url'] = 'https://getminera.com/api';
 $config['live_stats_url'] =  'app/stats';
 $config['stored_stats_url'] =  'app/api?command=history_stats&type=hourly';
